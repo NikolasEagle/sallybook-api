0c8d0d1f455f07fb69e76df77a29e768a7dd7e54
NikolasEagle <eagle.dev.stack@gmail.com>
2025-02-21 12:44:21 +0400

    Удаление файла app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const port = process.env.PORT;
-
-const app = express();
-
-app.listen(port, () => {
-  console.log(`Service is running on http://localhost:${port}`);
-});
