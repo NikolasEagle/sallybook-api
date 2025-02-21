d06dcb8ba65f7552cac9c9034a3c194e907120cd
NikolasEagle <eagle.dev.stack@gmail.com>
2025-02-21 12:43:07 +0400

    Добавление функционала извлечения книг

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const port = process.env.PORT || 8080;
 
+const hostGoogleBooks = process.env.HOST_API_GOOGLE_BOOKS;
+const apiKeyGoogleBooks = process.env.API_KEY_GOOGLE_BOOKS;
+
 const app = express();
 
-app.get("/", async (req: Request, res: Response) => {
-  res.send("Hello Cabtr");
+import getAllBooks from "./lib/getAllBooks.ts";
+
+app.get("/api/books/:pageId", async (req: Request, res: Response) => {
+  try {
+    const pageId: number = +req.params.pageId;
+
+    const result = await getAllBooks(
+      hostGoogleBooks,
+      apiKeyGoogleBooks,
+      pageId
+    );
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => {
---
+++ src/lib/getAllBooks.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+interface Response {
+  data: Body;
+}
+
+interface Body {
+  kind: string;
+
+  totalItems: number;
+
+  items: Object[] | [];
+}
+
+interface Books {
+  currentPage: number;
+
+  data: Object[] | [];
+
+  nextPage: number | null;
+}
+
+export default async function getAllBooks(
+  host: string | undefined,
+  key: string | undefined,
+  pageId: number
+): Promise<Books> {
+  const currentPage: number = pageId;
+
+  const startIndex: number = (currentPage - 1) * 30;
+
+  if (startIndex > 1000) {
+    return { currentPage: currentPage, data: [], nextPage: null };
+  }
+
+  const response: Response = await axios.get(
+    `${host}?key=${key}&q=intitle:&startIndex=${startIndex}&projection=lite&maxResults=30&language=ru-RU`
+  );
+
+  const body: Body = response.data;
+
+  console.log(body.totalItems);
+
+  if (body.items) {
+    const data = body.items;
+
+    return {
+      currentPage: currentPage,
+      data: data,
+      nextPage: currentPage < 17 ? currentPage + 1 : null,
+    };
+  }
+
+  return { currentPage: currentPage, data: [], nextPage: null };
+}
